15610e7c711a8caf6795aa9b41cde1fd6829b010
manfred-commits <manfredipiraino@gmail.com>
2022-01-23 18:57:09 +0100

    add: refactoring

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function randomNumber(num1, num2){
-    return Math.floor(Math.random() * (num2 - num1 + 1) + num1);;
-}
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}   
 
 // selects the tris output message
 let resultText= document.getElementById("tris_result");
@@ -21,12 +21,6 @@ let cells =[
     ["","",""],
     ["","",""]
 ];
-
-// for(let i=0;i<cells.length;i++){
-//     for(let j=0;i<cells[i].length;j++){
-        
-//     }
-// }
 
 
 let win= null;
@@ -64,58 +58,34 @@ function mouseClick(event){
         clickedCells.push(parseInt(event.target.value));
     
         cells[event.target.dataset.x][event.target.dataset.y]=event.target.innerHTML;
-        let randomX=0;
-        do{
-            randomX=parseInt(getRandomArbitrary(1, 9));
-        }while(clickedCells.includes(randomX) && clickedCells.length<8);
 
-        document.querySelector("button[value='"+randomX+"']").innerHTML=ai;
-        clickedCells.push(parseInt(randomX));
+        checkWinner();
 
-        cells[document.querySelector("button[value='"+randomX+"']").dataset.x][document.querySelector("button[value='"+randomX+"']").dataset.y]=ai;
-        document.querySelector("button[value='"+randomX+"']").classList.add("not-allowed");
+        if(win==null){
+            let randomX=0;
+            do{
+                randomX=parseInt(getRandomArbitrary(1, 9));
+            }while(clickedCells.includes(randomX) && clickedCells.length<8);
+            if(!clickedCells.includes(randomX)){
+                document.querySelector("button[value='"+randomX+"']").innerHTML=ai;
+                clickedCells.push(parseInt(randomX));
+        
+                cells[document.querySelector("button[value='"+randomX+"']").dataset.x][document.querySelector("button[value='"+randomX+"']").dataset.y]=ai;
+                document.querySelector("button[value='"+randomX+"']").classList.add("not-allowed");
+            }
+
+        }
 
 
     }
-    // if(event.target.innerHTML==trisPointer && !clickedCells.includes(parseInt(event.target.value))){
-        
-
-    //     if(trisPointer=="O"){
-
-    //         trisPointer="X";
-
-    //     }else if(trisPointer=="X"){
-
-    //         trisPointer="O";
-    //     }
-    // }else if(event.target.innerHTML=="" && trisPointer=="X"){
-
-    //     event.target.innerHTML=trisPointer;
-    //     trisPointer="O";
-
-    // }else if(event.target.innerHTML=="" && trisPointer=="O"){
-
-    //     event.target.innerHTML=trisPointer;
-    //     trisPointer="X";
-    // }
+    
     checkWinner();
-    // while(!clickedCells.includes(randomX)){
-        //     let randomX=getRandomInt(8);
-        
-        //     // document.querySelector("button[data-x='"+1+"'][data-y='"+1+"']").innerHTML='X';
-        
-        // }
-    
-    // console.log();
-    
-    
-    
-    
+       
     
 }
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
-  }
+}   
 function checkWinner(){
     for(let i=0; i<cells.length;i++){
         // check if it won orizonatally
@@ -152,15 +122,6 @@ function checkWinner(){
         
     }
 }
-
-function aiMove(){
-    for(let i=0; i<cells.length;i++){
-        cells[randomNumber(0,2)][randomNumber(0,2)];
-        
-    }
-}
-
-
 
 function removeEventTris(cells,index){
     containerTris.removeEventListener("click",mouseClick);
