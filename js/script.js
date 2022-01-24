93cc2dc472c69ec8ba16515081a8b8e18ad6b7b9
manfred-commits <manfredipiraino@gmail.com>
2022-01-24 08:22:55 +0100

    add: ai blocking move

--- js/script.js
+++ js/script.js
@@ -21,9 +21,15 @@ let cells =[
     ["","",""],
     ["","",""]
 ];
-
+let numeriCells=[
+    ["","",""],
+    ["","",""],
+    ["","",""]
+];
 
 let win= null;
+
+let aiTurn=true;
 
 // event listener on mouseover, to add the temporary tris pointer
 containerTris.addEventListener('mouseover',mouseOver);
@@ -53,6 +59,15 @@ containerTris.addEventListener('click', mouseClick
 function mouseClick(event){
     if(!clickedCells.includes(parseInt(event.target.value))){
 
+        
+        for(let i=0;i<cells.length;i++){
+            for(let j=0; j<cells[i].length;j++){
+                if(cells[i][j]!=''){ 
+                    document.querySelector("button[data-x='"+i+"'][data-y='"+j+"']").innerHTML=cells[i][j];
+                    document.querySelector("button[data-x='"+i+"'][data-y='"+j+"']").classList.add("not-allowed");
+                }
+            }
+        }
         event.target.innerHTML=player;
         event.target.classList.add("not-allowed");
         clickedCells.push(parseInt(event.target.value));
@@ -60,21 +75,39 @@ function mouseClick(event){
         cells[event.target.dataset.x][event.target.dataset.y]=event.target.innerHTML;
 
         checkWinner();
+        aiTurn=true;
+        console.log(aiTurn);
+        console.log(numeriCells);
 
-        if(win==null){
-            let randomX=0;
-            do{
-                randomX=parseInt(getRandomArbitrary(1, 9));
-            }while(clickedCells.includes(randomX) && clickedCells.length<8);
-            if(!clickedCells.includes(randomX)){
-                document.querySelector("button[value='"+randomX+"']").innerHTML=ai;
-                clickedCells.push(parseInt(randomX));
-        
-                cells[document.querySelector("button[value='"+randomX+"']").dataset.x][document.querySelector("button[value='"+randomX+"']").dataset.y]=ai;
-                document.querySelector("button[value='"+randomX+"']").classList.add("not-allowed");
+        blockingWinningAiMove();
+        console.log(aiTurn);
+        console.log(numeriCells);
+        for(let i=0;i<cells.length;i++){
+            for(let j=0; j<cells[i].length;j++){
+                if(cells[i][j]!=''){ 
+                    document.querySelector("button[data-x='"+i+"'][data-y='"+j+"']").innerHTML=cells[i][j];
+                    document.querySelector("button[data-x='"+i+"'][data-y='"+j+"']").classList.add("not-allowed");
+                }
             }
-
         }
+
+        console.log(cells);
+
+        // random ai turn
+        // if(win==null){
+        //     let randomX=0;
+        //     do{
+        //         randomX=parseInt(getRandomArbitrary(1, 9));
+        //     }while(clickedCells.includes(randomX) && clickedCells.length<8);
+        //     if(!clickedCells.includes(randomX)){
+        //         document.querySelector("button[value='"+randomX+"']").innerHTML=ai;
+        //         clickedCells.push(parseInt(randomX));
+        
+        //         cells[document.querySelector("button[value='"+randomX+"']").dataset.x][document.querySelector("button[value='"+randomX+"']").dataset.y]=ai;
+        //         document.querySelector("button[value='"+randomX+"']").classList.add("not-allowed");
+        //     }
+
+        // }
 
 
     }
@@ -128,4 +161,125 @@ function removeEventTris(cells,index){
     containerTris.removeEventListener("mouseout",mouseLeave);
     containerTris.removeEventListener("mouseover",mouseOver);
         
+}
+
+
+function blockingWinningAiMove() {
+
+    for(let i=0;i<cells.length;i++){
+        for(let j=0; j<cells[i].length;j++){
+            if(cells[i][j]=='X'){ 
+                numeriCells[i][j]=3;
+            }else if(cells[i][j]=='O'){
+                numeriCells[i][j]=5;
+            }else if(cells[i][j]==''){
+                numeriCells[i][j]=1;
+            }
+        }
+    }
+    for(let i=0;i<cells.length;i++){
+        for(let j=0; j<cells[i].length;j++){
+
+
+        // blocking move
+        // check orizonatally
+        if(numeriCells[i][0]*numeriCells[i][1] *numeriCells[i][2]==25 && aiTurn){
+           if(numeriCells[i][0]==1) {
+                numeriCells[i][0]=3;
+                aiTurn=false;
+           }else if(numeriCells[i][1]==1){
+                numeriCells[i][1]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[i][2]==1){
+                numeriCells[i][2]=3;
+                aiTurn=false;
+
+           }
+        }
+        // check vertically
+        if(numeriCells[0][i]*numeriCells[1][i]*numeriCells[2][i]==25 && aiTurn){
+            if(numeriCells[0][i]==1) {
+                numeriCells[0][i]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[1][i]==1){
+                numeriCells[1][i]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[2][i]==1){
+                numeriCells[2][i]=3;
+                aiTurn=false;
+
+           }
+        }
+        // check diagonally
+        if((numeriCells[0][0]*numeriCells[1][1]*numeriCells[2][2]==25 && aiTurn) ){
+            if(numeriCells[0][0]==1) {
+                numeriCells[0][0]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[1][1]==1){
+                numeriCells[1][1]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[2][2]==1){
+                numeriCells[2][2]=3;
+                aiTurn=false;
+
+           }
+            
+        }
+        if((numeriCells[2][0]*numeriCells[1][1]*numeriCells[0][2]==25 && aiTurn) ){
+            if(numeriCells[2][0]==1) {
+                numeriCells[2][0]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[1][1]==1){
+                numeriCells[1][1]=3;
+                aiTurn=false;
+
+           }else if(numeriCells[2][2]==1){
+                numeriCells[0][2]=3;
+                aiTurn=false;
+
+           }
+            
+        }
+
+        //winning move
+
+        // check orizonatally
+        if(numeriCells[i][0]*numeriCells[i][1] *numeriCells[i][2]==9){
+            
+        }
+        // check vertically
+        if(numeriCells[0][i]*numeriCells[1][i]*cells[2][i]==9){
+            
+        }
+        // checkdiagonally
+        if((numeriCells[0][0]*numeriCells[1][1]*numeriCells[2][2]==9) ){
+        
+            
+        }
+        if((numeriCells[2][0]*numeriCells[1][1]*numeriCells[0][2]==9) ){
+            
+            
+        }
+        
+    }
+    for(let i=0;i<cells.length;i++){
+        for(let j=0; j<cells[i].length;j++){
+            if(numeriCells[i][j]==3){ 
+                cells[i][j]='X';
+            }else if(numeriCells[i][j]==5){
+                cells[i][j]='O';
+            }else if(numeriCells[i][j]==1){
+                cells[i][j]='';
+            }
+        }
+    }
+
+    }
+    console.log(numeriCells);
 }
